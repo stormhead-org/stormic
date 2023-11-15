b82ed1155a1918d739a1893a4896f8ee2cdfc882
Pavel <gamenimsi@gmail.com>
2023-11-15 12:42:07 +0300

    up

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,10 +1,11 @@
+'use client'
+
 import dynamic from 'next/dynamic'
 import MainLayout from '../../components/ui/custom_layouts/main_layout/MainLayout'
 import styles from './page.module.scss'
 
 const Editor = dynamic(
-	() =>
-		import('../../components/pages/write/editor/Editor').then(m => m.Editor),
+	() => import('../../components/pages/write/editor/Editor'),
 	{ ssr: false }
 )
 
@@ -28,13 +29,7 @@ const WritePage: React.FC<WriteFormPropsHeader> = ({ title }) => {
 								placeholder='Заголовок'
 								defaultValue={title}
 							/>
-							<div className={styles.Editor}>{/* <Editor /> */}</div>
-						</div>
-						<div className={styles.BotRow}>
-							<input
-								className={styles.NewPostBody}
-								placeholder='Нажмите Tab для выбора инструмента'
-							/>
+							<div className={styles.Editor}>{<Editor />}</div>
 						</div>
 					</div>
 				</div>
--- src/components/pages/write/editor/Editor.tsx
+++ src/components/pages/write/editor/Editor.tsx
@@ -1,20 +1,70 @@
+import Code from '@editorjs/code'
 import EditorJS from '@editorjs/editorjs'
-import React from 'react'
+import Header from '@editorjs/header'
+import React, { useRef } from 'react'
 
-export const Editor: React.FC = () => {
+const Editor: React.FC = () => {
+	const editorRef = useRef<EditorJS | null>(null)
 	React.useEffect(() => {
-		const editor = new EditorJS({
-			holder: 'editor'
-		})
+		if (!editorRef.current?.isReady) {
+			const editor = new EditorJS({
+				holder: 'editor',
+				minHeight: 0,
+				// autofocus: true,
+				placeholder: 'Введите текст вашей статьи',
+				hideToolbar: false,
+				tools: {
+					code: Code,
+					header: {
+						class: Header,
+						/**
+						 * This property will override the common settings
+						 * That means that this tool will have only Marker and Link inline tools
+						 * If 'true', the common settings will be used.
+						 * If 'false' or omitted, the Inline Toolbar wont be shown
+						 */
+						inlineToolbar: ['marker', 'link'],
+						config: {
+							placeholder: 'Header'
+						},
+						shortcut: 'Tab'
+					}
+				},
+				i18n: {
+					messages: {
+						ui: {
+							// Translations of internal UI components of the editor.js core
+						},
+						toolNames: {
+							// Section for translation Tool Names: both block and inline tools
+						},
+						tools: {
+							// Section for passing translations to the external tools classes
+							// The first-level keys of this object should be equal of keys ot the 'tools' property of EditorConfig
+						},
+						blockTunes: {
+							// Section allows to translate Block Tunes
+						}
+					}
+				}
+			})
+
+			editorRef.current = editor
+		}
 
 		return () => {
-			editor.isReady
-				.then(() => {
-					editor.destroy()
-				})
-				.catch(e => console.error('ERROR editor cleanup', e))
+			if (editorRef.current && editorRef.current.destroy) {
+				editorRef.current.destroy()
+			}
 		}
 	}, [])
 
-	return <div id='editor' />
+	return (
+		<div
+			id='editor'
+			// ref={editorRef}
+		/>
+	)
 }
+
+export default Editor
