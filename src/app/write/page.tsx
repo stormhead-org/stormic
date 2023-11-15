52c744279ed02702ef8467e23f4680d294aa11f0
Pavel <gamenimsi@gmail.com>
2023-11-15 17:57:25 +0300

    up

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,19 +1,51 @@
 'use client'
 
-import dynamic from 'next/dynamic'
+import EditorJS from '@editorjs/editorjs'
+import React from 'react'
 import MainLayout from '../../components/ui/custom_layouts/main_layout/MainLayout'
 import styles from './page.module.scss'
 
-const Editor = dynamic(
-	() => import('../../components/pages/write/editor/Editor'),
-	{ ssr: false }
-)
+// const Editor = dynamic(
+// 	() => import('../../components/pages/write/editor/Editor'),
+// 	{ ssr: false }
+// )
 
 interface WriteFormPropsHeader {
 	title?: string
 }
 
+// const WritePage: React.FC<WriteFormPropsHeader> = ({ title }) => {
+// 	return (
+// 		<>
+// 			<MainLayout
+// 				className='WriteLayoutSettings'
+// 				hideLeftSideBar
+// 				hideRightSideBar
+// 			>
+// 				<div className={styles.WritePage}>
+// 					<div className={styles.WritePageFrame}>
+// 						<div className={styles.TopRow}>
+// 							<input
+// 								className={styles.NewPostName}
+// 								placeholder='Заголовок'
+// 								defaultValue={title}
+// 							/>
+// 							<div className={styles.Editor}>{<Editor />}</div>
+
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</MainLayout>
+// 		</>
+// 	)
+// }
+
 const WritePage: React.FC<WriteFormPropsHeader> = ({ title }) => {
+	React.useEffect(() => {
+		const editor = new EditorJS({
+			holder: 'editor'
+		})
+	}, [])
 	return (
 		<>
 			<MainLayout
@@ -29,7 +61,8 @@ const WritePage: React.FC<WriteFormPropsHeader> = ({ title }) => {
 								placeholder='Заголовок'
 								defaultValue={title}
 							/>
-							<div className={styles.Editor}>{<Editor />}</div>
+
+							<div id='editor' />
 						</div>
 					</div>
 				</div>
