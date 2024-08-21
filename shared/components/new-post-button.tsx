44b43bd1f368a22a48dd7913779bcba7e5ae627d
nimscore <nimscore@gmail.com>
2024-08-21 16:23:09 +0300

    feat: add editorjs & modal for him. Add write post components

--- shared/components/new-post-button.tsx
+++ shared/components/new-post-button.tsx
@@ -1,24 +1,49 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { AuthModal } from '@/shared/components/modals'
+import { WriteModal } from '@/shared/components/modals/write-modal'
 import React from 'react'
 import { cn } from '../lib/utils'
 import { Button } from './ui/button'
 
 interface Props {
+	authorAvatar: string
+	authorName: string
+	authorUrl: string
+	hasSession: boolean
 	className?: string
 }
 
-export const NewPostButton: React.FC<Props> = ({ className }) => {
-	const router = useRouter()
-
+export const NewPostButton: React.FC<Props> = ({
+	                                               authorAvatar,
+	                                               authorName,
+	                                               authorUrl,
+	                                               hasSession,
+	                                               className
+                                               }) => {
+	const [openWriteModal, setOpenWriteModal] = React.useState(false)
+	const [openAuthModal, setOpenAuthModal] = React.useState(false)
+	
 	return (
 		<div className={cn('', className)}>
+			<WriteModal
+				open={openWriteModal}
+				onClose={() => setOpenWriteModal(false)}
+				authorAvatar={authorAvatar}
+				authorName={authorName}
+				authorUrl={authorUrl}
+			/>
+			
+			<AuthModal
+				open={openAuthModal}
+				onClose={() => setOpenAuthModal(false)}
+			/>
+			
 			<Button
 				variant='secondary'
 				className='h-12 w-full text-lg font-bold'
 				type='button'
-				onClick={() => router.push('/write')}
+				onClick={hasSession ? () => setOpenWriteModal(true) : () => setOpenAuthModal(true)}
 			>
 				Новый пост
 			</Button>
