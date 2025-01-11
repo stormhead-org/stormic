fa68b937a9bde6940a10175e29c405545f5ba4a1
nims <nimscore@gmail.com>
2025-01-11 23:11:30 +0300

    feat: func for upload logo & banner cumminities in s3

--- shared/components/modals/new-community-modal/forms/style-form.tsx
+++ shared/components/modals/new-community-modal/forms/style-form.tsx
@@ -1,12 +1,21 @@
+'use client'
+
 import { Title } from '@/shared/components'
-import { FormInput } from '@/shared/components/form/'
+import { FileUpload } from '@/shared/components/file-upload'
 import { Button } from '@/shared/components/ui/button'
+import { Form, FormControl, FormField, FormItem } from '@/shared/components/ui/form'
+import { useModal } from '@/shared/hooks/use-modal-store'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { Plus } from 'lucide-react'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import qs from 'query-string'
 import React from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { useIntl } from 'react-intl'
 import { toast } from 'sonner'
+import * as z from 'zod'
 import { formLoginSchema, TFormLoginValues } from './schemas'
 
 interface Props {
@@ -16,45 +25,59 @@ interface Props {
 	>
 }
 
+const formSchema = z.object({
+	fileUrl: z.string().min(1, {
+		message: 'Прикрепите файл'
+	})
+})
+
 export const CommunityStyleForm: React.FC<Props> = ({ setType, onClose }) => {
 	const { formatMessage } = useIntl()
-	const form = useForm<TFormLoginValues>({
-		resolver: zodResolver(formLoginSchema),
+	
+	const { type, data } = useModal()
+	const router = useRouter()
+	
+	const { apiUrl, query } = data
+	
+	const formLogo = useForm({
+		resolver: zodResolver(formSchema),
 		defaultValues: {
-			email: '',
-			password: ''
+			fileUrl: ''
 		}
 	})
-
-	const onSubmit = async (data: TFormLoginValues) => {
+	
+	const formBanner = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			fileUrl: ''
+		}
+	})
+	
+	const isLoading = formLogo.formState.isSubmitting || formBanner.formState.isSubmitting
+	
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			const resp = await signIn('credentials', {
-				...data,
-				redirect: false
+			const url = qs.stringifyUrl({
+				url: apiUrl || '',
+				query
 			})
-
-			if (!resp?.ok) {
-				throw Error()
-			}
-
-			toast.success(String(formatMessage({ id: 'loginForm.toastSuccess' })), {
-				icon: '✅'
+			
+			await axios.post(url, {
+				...values,
+				content: values.fileUrl
 			})
-
-			onClose?.()
+			
+			formLogo.reset()
+			formBanner.reset()
+			router.refresh()
 		} catch (error) {
-			console.error('Error [LOGIN]', error)
-			toast.error(String(formatMessage({ id: 'loginForm.toastError' })), {
-				icon: '❌'
-			})
+			console.log(error)
 		}
 	}
 
 	return (
-		<FormProvider {...form}>
-			<form
+			<div
 				className='flex flex-col gap-4'
-				onSubmit={form.handleSubmit(onSubmit)}
 			>
 				<div className='flex justify-between items-center'>
 					<div className='mr-2'>
@@ -70,43 +93,74 @@ export const CommunityStyleForm: React.FC<Props> = ({ setType, onClose }) => {
 						</p>
 					</div>
 				</div>
-
-				<FormInput
-					name='name'
-					label='Название сообщества'
-					placeholder='Казахстан'
-					required
-				/>
-
-				<FormInput
-					name='slug'
-					label='Ссылка на сообщество (SLUG)'
-					placeholder='kazakhstan-foreva'
-					required
-				/>
-
-				<FormInput
-					name='description'
-					label='Описание сообщества'
-					type='text'
-					placeholder='Лучшее сообщество северного казахстана'
-					required
-				/>
-
+				
+				<Form {...formLogo}>
+					<form onSubmit={formLogo.handleSubmit(onSubmit)}>
+						<p className='flex justify-center'>
+							Логотип
+						</p>
+						<div className='flex items-center justify-center text-center mt-2'>
+							<FormField
+								control={formLogo.control}
+								name='fileUrl'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<FileUpload
+												endpoint='messageFile'
+												value={field.value}
+												onChange={field.onChange}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+					</form>
+				</Form>
+				
+				<Form {...formBanner}>
+					<form onSubmit={formBanner.handleSubmit(onSubmit)}>
+							<p className='flex justify-center'>
+								Баннер
+							</p>
+							<div className='flex items-center justify-center text-center mt-2'>
+								<FormField
+									control={formBanner.control}
+									name='fileUrl'
+									render={({ field }) => (
+										<FormItem>
+											<FormControl>
+												<FileUpload
+													endpoint='serverImage'
+													value={field.value}
+													onChange={field.onChange}
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+							</div>
+					</form>
+				</Form>
+				
+				
+				
 				<div className='flex justify-center gap-4'>
 					<Button
 						variant='blue'
-						loading={form.formState.isSubmitting}
+						loading={formLogo.formState.isSubmitting || formBanner.formState.isSubmitting}
 						className='flex items-center gap-2 text-sm font-bold w-1/2'
 						type='submit'
 						onClick={() => setType('name')}
 					>
 						Назад
 					</Button>
-
+					
 					<Button
 						variant='blue'
-						loading={form.formState.isSubmitting}
+						loading={formLogo.formState.isSubmitting || formBanner.formState.isSubmitting}
+						disabled={isLoading}
 						className='flex items-center gap-2 text-sm font-bold w-1/2'
 						type='submit'
 						onClick={() => setType('style')}
@@ -114,7 +168,6 @@ export const CommunityStyleForm: React.FC<Props> = ({ setType, onClose }) => {
 						Далее
 					</Button>
 				</div>
-			</form>
-		</FormProvider>
+			</div>
 	)
 }
